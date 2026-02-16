289f25d9fb41f32afe023adb96fd1786bd7ffdac
wkallil <kallil.tim@gmail.com>
2026-02-16 16:15:17 -0300

    build: configura estrutura inicial do projeto e ambiente Maven
    - Inicializa o modelo de objetos do projeto (pom.xml) e dependências
    - Adiciona classe principal 'RunIt' e configurações de aplicação (YAML)
    - Configura Maven Wrapper e políticas de versionamento (.gitignore, .gitattributes)

---
+++ src/main/java/com/wkallil/RunIt.java
@@ -0,0 +1,13 @@
+package com.wkallil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunIt {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunIt.class, args);
+	}
+
+}
